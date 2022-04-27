073c02cd35a00037c069d7855f5fb0b7937e08a9
BoastfulApex <jahongirnormuminov8@gmail.com>
2022-04-27 23:48:17 +0500

    last-2

--- Project/settings.py
+++ Project/settings.py
@@ -71,14 +71,25 @@ WSGI_APPLICATION = 'Project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+   'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'df6836u0e99ima',
+        'USER': 'zxvudssmmjgboa',
+        'PASSWORD': 'ed4735351bc80e26705928d63fcbe4361dd7c0af46a1dc4962f269aeebf3ca52',
+        'HOST': 'ec2-35-175-19-96.compute-1.amazonaws.com',
+        'PORT': '5432'
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
